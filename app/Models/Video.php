0e2299950a1f2e750735cba61f1ed00331b9a2ba
drewwm5 <drewmwilliams55@gmail.com>
2023-11-16 22:35:15 -0800

    Updated video model

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\DB;
+use Laravel\Scout\Searchable;
 
 class Video extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $primaryKey = 'video_id';
     public $incrementing = false;
@@ -25,6 +27,19 @@ class Video extends Model
         'total_jobs',
         'subtitle_updated_at'
     ];
+
+    public function toSearchableArray()
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description
+        ];
+    }
+
+    public function captions(): HasMany 
+    {
+        return $this->hasMany(Caption::class);
+    }
 
     private static function addJobBatchesToVideo(Collection $videos): Collection {
         foreach ($videos as $index => $video) {
