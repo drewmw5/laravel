fc0e0d9df8900a071a2b8cb873c081d9924cde4b
drewwm5 <drewmwilliams55@gmail.com>
2023-11-16 22:35:09 -0800

    Updated Caption model

--- app/Models/Caption.php
+++ app/Models/Caption.php
@@ -3,18 +3,65 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Laravel\Scout\Searchable;
 
 class Caption extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
-    public $timestamps = false;
+    protected $primaryKey = 'id';
     
+    public $timestamps = false;
+
     protected $fillable = [
         'video_id',
         'text',
         'start',
         'duration',
     ];
+
+    public function toSearchableArray()
+    {
+        return [
+            'text' => $this->text,
+        ];
+    }
+
+    public function video(): BelongsTo
+    {
+        return $this->belongsTo(Video::class, 'video_id');
+    }
+
+    public function next() 
+    {
+        return static::where('video_id', $this->video_id) 
+            ->where('start', '>', 1)
+            ->orderBy('start', 'ASC')
+            ->first();
+    }
+
+    public function previous()
+    {
+        return static::where('video_id', $this->video_id)
+            ->where('start', '<', $this->start)
+            ->orderBy('start', 'DESC')
+            ->first();
+    }
+
+    // public static function search($query)
+    // {
+    //     $captions = Caption::where('text', 'LIKE', "%$query%")
+    //         ->orderBy('video_id')
+    //         ->orderBy('start')
+    //         ->select('start', 'duration', 'text', 'video_id')
+    //         ->get()
+    //         ->groupBy('video_id')
+    //         ->forPage(1, 5);
+
+    //     return $captions;
+    // }
 }
